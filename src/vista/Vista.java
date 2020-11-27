c4705eb2eb2e691f4c5f8b20ed1b6d00349ed142
angellucruz16 <angellucruz16@hotmail.com>
2020-11-27 11:20:58 -0500

    COMMIT11
    Avances en actualizacion

--- src/vista/Vista.java
+++ src/vista/Vista.java
@@ -1,5 +1,64 @@
 package vista;
 
-public class Vista {
+import controlador.Controlador;
+import modelo.Main;
+import modelo.Persona;
+//5
+import processing.core.PApplet;
+public class Vista extends PApplet{
 
-}
+	Bolita [] bolitas;
+	Controlador controlador;
+	Main modelo;
+	
+	public void settings() {
+		
+		size(700, 700);
+		modelo=new Main();
+		controlador=new Controlador(modelo);
+		crearBolitas();
+		
+	} //SETTINGS
+	
+	public void setup() {
+		noStroke();
+		frameRate(80);
+		ellipseMode(RADIUS);
+
+	} //SETUP
+	
+	public void draw() {	
+	} //DRAW
+	
+	public void crearBolitas() {
+		Persona[] arrPersonas = controlador.getPersonas();
+		bolitas = new Bolita[arrPersonas.length];
+		int i=0;
+		for (Persona p: arrPersonas) {
+			bolitas[i]= new Bolita(p.getId(), this, p.getInfectado(), p.getSano(), p.getRecuperado(), controlador);
+			i++;
+		}
+		
+	} //CREAR BOLITAS
+	
+	public void actualizarBolitas() {
+		
+		for (Persona persona : controlador.actualizar()) {
+			for (Bolita bolita : bolitas) {
+				if (bolita.id==persona.getId() && persona.getRecuperado()) {
+					bolita.azul();
+				}
+			}
+		}
+	} // ACTUALIZAR BOLITAS
+	
+
+	public static void main(String[] args) {
+		String[] processingArgs = {"Main"};
+		Vista mySketch = new Vista();
+		PApplet.runSketch(processingArgs, mySketch);
+		//PApplet.main("process.Pro");
+	}
+	
+} //CLASE
+
